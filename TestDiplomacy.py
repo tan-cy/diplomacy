f423008253ae606f3bd27396eadef4b390e1253c
Florencia Froebel <florencia.froebel@gmail.com>
2020-07-06 13:18:02 -0500

    fixed the template for TestDiplomacy.py

--- TestDiplomacy.py
+++ TestDiplomacy.py
@@ -6,3 +6,42 @@
 
 from io import StringIO
 from unittest import main, TestCase
+
+from Diplomacy import diplomacy_read, diplomacy_print, diplomacy_solve
+
+# --------------
+# TestDiplomacy
+# --------------
+
+class TestDiplomacy(TestCase):
+
+    def test_read_1(self):
+        pass
+
+    def test_read_2(self):
+        pass
+
+    def test_read_3(self):
+        pass
+
+    def test_print_1(self):
+        pass
+
+    def test_print_2(self):
+        pass
+
+    def test_print_3(self):
+        pass
+
+    def test_solve_1(self):
+        pass
+
+    def test_solve_2(self):
+        pass
+
+    def test_solve_3(self):
+        pass
+
+
+if __name__ == "__main__":
+    main()
