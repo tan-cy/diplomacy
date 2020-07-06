8cb7bd7650b2bda67cb86b0f49fc1893a6a6f766
Cindy Tan <ctan@utexas.edu>
2020-07-06 12:49:29 -0500

    template

---
+++ RunDiplomacy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -------
+# imports
+# -------
+
+import sys
+from Diplomacy import diplomacy_solve
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+    diplomacy_solve(sys.stdin, sys.stdout)
---
+++ TestDiplomacy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# -------
+# imports
+# -------
+
+from io import StringIO
+from unittest import main, TestCase
