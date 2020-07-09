6de6f391fc779b821105a4443a23cf614a4e19a1
Cindy Tan <ctan@utexas.edu>
2020-07-09 18:22:16 -0500

    added dictionaries for attackers/supporters, list for all armies

--- Diplomacy.py
+++ Diplomacy.py
@@ -52,12 +52,47 @@ def diplomacy_find_support(l, d):
            d.update({l[3]: 1})
     return d
 
+# ----------------------
+# diplomacy_find_start
+# ----------------------
+
+
+def diplomacy_find_start(l, d):
+    """
+    returns a updated dictionary of starting city for an army
+    l is a list of strings : ["A", "Barcelona", "Move", "Madrid"]
+    """
+    return d.update({l[0]:l[1]})
+
+
 # -------------
 # diplomacy_eval
 # -------------
-def diplomacy_eval():
-    pass
-    
+
+
+def diplomacy_eval(supporters, attackers, start, armies):
+    """
+    returns a list of outcomes
+    supporters is a dictionary {army : # of support}
+    attackers is a dictionary {army : city attacking} 
+    l is a list
+    """
+    outcome = {}
+    for army in supporters:
+        if army[0] in attackers
+        """
+        if army[2] == "Move":
+            support_attacker = supporters.get(army[0])
+            support_defender = supporters.get(army[3])
+            if support_attacker > support_defender:
+                outcome.update({army[0]: army[3])
+                outcome.update({army[3]: "[dead]")
+            elif support_attacker < support_defender:
+                outcome.update({army[0]: "[dead]")
+            else:
+                outcome.update({army[0]: "[dead]")
+                outcome.update({army[3]: "[dead]")
+        """
 # -------------
 # diplomacy_solve
 # -------------
@@ -69,8 +104,19 @@ def diplomacy_solve(r, w):
     w a writer
     """
     supporters = {}
+    attackers = {}
+    armies = []
+    start = {}
+    # finds initial moves 
     for s in r:
         l = diplomacy_read(s)
         supporters = diplomacy_find_support(l, supporters)
-        i, j = diplomacy_eval(l)
-        diplomacy_print(w, i, j)
+        attackers.update({l[0]:l[3]) # army name : city attacking
+        start = diplomacy_find_start(l, start)
+        armies.append(l)
+    # finds solution after move    
+    solution = diplomacy_eval(supporters, attackers, start, armies)
+        
+        
+        #diplomacy_print(w, i, j)
+    
